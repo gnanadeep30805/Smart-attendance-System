03d2014549d6f35f6055d92af54ec7361f8c101f
gnanadeep30805 <deepu33025@gmail.com>
2026-03-11 09:13:46 +0530

    updated frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import AuthProvider from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
